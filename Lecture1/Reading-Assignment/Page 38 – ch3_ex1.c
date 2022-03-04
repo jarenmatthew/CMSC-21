05521419662ccbc450a2b820ae508ca2b087a975
jarenmatthew <99801133+jarenmatthew@users.noreply.github.com>
2022-03-04 18:00:56 +0800

    Update Page 38 – ch3_ex1.c

--- Lecture1/Reading-Assignment/Page 38 – ch3_ex1.c
+++ Lecture1/Reading-Assignment/Page 38 – ch3_ex1.c
@@ -1,15 +1,19 @@
+// includes directive
 #include <stdio.h>
 
 int main(void){
-
+    
+    //declares multiple variables
     int i , j;
     float x, y;
 
+    //assigns value to variables
     i = 10;
     j = 20;
     x = 43.2892f;
     y = 5527.0f;
 
+    //prints result
     printf("i = %d, j = %d, x = %f, y = %f\n",i,j,x,y);
 
 }
