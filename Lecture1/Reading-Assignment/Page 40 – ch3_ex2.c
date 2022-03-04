193f6bad8f36d09d703c1dfd214900859c4ec15a
jarenmatthew <99801133+jarenmatthew@users.noreply.github.com>
2022-03-04 18:02:57 +0800

    Update Page 40 – ch3_ex2.c

--- Lecture1/Reading-Assignment/Page 40 – ch3_ex2.c
+++ Lecture1/Reading-Assignment/Page 40 – ch3_ex2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main(void){
-
+    
+    //declares variables
     int i;
     float x;
 
+    //assigns value to variables
     i = 40;
     x = 839.21f;
 
+    //prints result
     printf("|%d|%5d|%-5d|%5.3d|\n",i,i,i,i);
     printf("|%10.3f|%10.3e|%-10g|\n",x,x,x);
 
